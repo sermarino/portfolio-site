616b5fae90cb3d78f969a6bde19b732fd068f266
Sergio Marino <smarino@exis.it>
2025-10-16 16:08:25 +0200

    Changed text inside button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen grid place-items-center bg-gray-50">
       <button className="px-4 py-2 rounded-lg border hover:bg-gray-100">
-        Ciao Tailwind 👋
+        Ciao Mondo!! 👋
       </button>
     </main>
   );
